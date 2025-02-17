b66b7ff418f6367bf72aa253ca3cd0b4916ab3bb
yasindu2000 <yasindudahanayake@gmail.com>
2025-02-17 15:44:04 +0530

    edit student.model.js

--- models/student.model.js
+++ models/student.model.js
@@ -10,6 +10,16 @@ var studentSchema = new mongoose.Schema({
     email: {
         type: String,
         required: 'this field is required'
+    },
+    mobile:{
+        type: Number,
+        required: 'this field is required'
+    },
+    city:{
+        type: String,
+        required: 'this field is required'
     }
 
 })
+
+mongoose.model("Student", studentSchema)
