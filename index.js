0734769d35de7f7eccf15f40d638977720763c45
yasindu2000 <yasindudahanayake@gmail.com>
2025-02-17 11:14:34 +0530

    Added project files

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const path = require("path")
+const handlebars = require("handlebars")
+const exphbs = require("express-handlebars")
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const bodyparser = require('body-parser')
+const bodyParser = require("body-parser")
+
+var app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.get("/", (req,res)=>{
+
+    res.send(`
+        <h2>welcome to school database</h2>
+        <h3>click here to  get access to the <b><a href="/student/list">Database</a></b3></h3>`
+    )
+})
+
+
+
+
+
+app.listen(3000, ()=>{
+
+    console.log("server unning..");
+})
